11334492539574f544e0c92aeec6e9b97a556450
SuperYuIuo <39399442+SuperYuIuo@users.noreply.github.com>
2023-04-27 21:07:31 +0800

    Update tm.sh

--- tm.sh
+++ tm.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 #FROM https://github.com/spiritLHLS/traffmonetizer-one-click-command-installation
 
+utf8_locale=$(locale -a 2>/dev/null | grep -i -m 1 -E "UTF-8|utf8")
+if [[ -z "$utf8_locale" ]]; then
+  echo "No UTF-8 locale found"
+else
+  export LC_ALL="$utf8_locale"
+  export LANG="$utf8_locale"
+  export LANGUAGE="$utf8_locale"
+  echo "Locale set to $utf8_locale"
+fi
+
 # 定义容器名
 NAME='tm'
 
